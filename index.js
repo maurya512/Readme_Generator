da071e62d636f99d34e707f95e1245413f80a810
Maurya Patel <mauryapatel@Chaitrangis-MacBook-Pro.local>
2020-08-12 22:31:10 -0400

    re did the file with new questions and put them all inside a function

--- index.js
+++ index.js
@@ -1,63 +1,60 @@
-var inquirer = require("inquirer");
-var fs = require('fs');
+// import modules 
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const writeFileAsync = util.promisify(fs.writeFile);
 
-// array of questions for user
-const questions = [
-{
-    type: "input",
-    message: "Enter the title of your project.",
-    name: "projectTitle"
-},
-{
-    type: "input",
-    message: "Provide project description",
-    name: "projectDescription"
-},
-{
-    type: "input",
-    message: "Please explain the step-by-step installation process of this project.",
-    name: "installationProcess"
-},
-{
-    type: "input",
-    message: "Provide user instructions to use the project.",
-    name: "userInstruction"
-},
-{
-    type: "input",
-    message: "Provide License name or badge.",
-    name: "projectLicense"
-},
-{
-    type: "input",
-    message: "Please provide with a contributers' names.",
-    name: "projectContributers"
-},
-{
-    type: "input",
-    message: "Please provide project tests.",
-    name: "projectTests"
-},
-{
-    type: "input",
-    message: "Enter your username.",
-    name: "userName"
-},
-{
-    type: "input",
-    message: "Please provide your github repo link.",
-    name: "projectRepo"
+// A function that prompts the user to ask the questions 
+function askQuestions() {
+    // array of objects
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the proejct?",
+            name: "projectTitle"
+        },
+        {
+            type: "input",
+            message: "Please enter a description of the project.",
+            name: "projectDescription"
+        },
+        {
+            type: "input",
+            message: "What are the installation steps for this project?",
+            name: "projectInstructions"
+        },
+        {
+            type: "input",
+            message: "What will this project be used for?",
+            name: "projectUsage"
+        },
+        {
+            type: "input",
+            message: "Who gets credit for the initial deployment of this project?",
+            name: "projectDeployer"
+        },
+        {
+            type: "list",
+            message: "Enter the license for this project",
+            choices: [
+                "Apache",
+                "MIT",
+                "ISC",
+                "GNU GPLv3",
+            ],
+            name: "projectLicense"
+        },
+        {
+            type: "input",
+            message: "Enter your github username",
+            name: "userName"
+        },
+        {
+            type: "input",
+            message: "Enter your email id.",
+            name: "userEmail"
+        },
+    ])
 }
-];
-
-// function to write README file
-function writeToFile(fileName, data) {
-}
-
-// function to initialize program
-function init() {
-
-}
-
-// function call to initialize program
-init();
+// just a console log to check no issues pertain thus far
+console.log("test ran successfully");
